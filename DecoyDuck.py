6ab9eeb325add7c73b5966163fea8652a6d9bd56
Aman29042000 <131285562+Aman29042000@users.noreply.github.com>
2023-04-19 22:39:04 +0530

    Add files via upload

---
+++ DecoyDuck.py
@@ -0,0 +1,17 @@
+## E22MCAG0021
+## Aman
+
+
+from Duck import Duck
+from FlyNoWay import FlyNoWay
+from MuteQuack import MuteQuack
+
+
+class DecoyDuck(Duck):
+    def __init__(self):
+        super().__init__()
+        self.fly_behavior = FlyNoWay()
+        self.quack_behavior = MuteQuack()
+
+    def display(self):
+        print("Duck Decoy")
